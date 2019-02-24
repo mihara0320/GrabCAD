a15c7668a0b039f5de84d7fc8f6d670b6fc484cc
Masaki <masaki0320@icloud.com>
2019-02-24 21:12:26 +0200

    SignalR Hub design

---
+++ GrabCAD.API/SignalR/GameHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrabCAD.API.Helpers;
+using GrabCAD.API.Models;
+using GrabCAD.API.ViewModels;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GrabCAD.API.Services
+{
+    public interface IGameHubClient
+    {
+        Task RecieveAnswer(AnswerViewModel model);
+    }
+    public class GameHub : Hub<IGameHubClient>
+    {
+        private IUserManager _userManager;
+
+        public GameHub(IUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public string GetConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            _userManager.Add(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            _userManager.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
