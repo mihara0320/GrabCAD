f3857f7b0c19cf157331df023c071d931e83d5d4
Masaki <masaki0320@icloud.com>
2019-02-24 22:46:27 +0200

    MathChallegne Generator added

---
+++ GrabCAD.API/Helpers/MathChallengeGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrabCAD.API.Models;
+
+namespace GrabCAD.API.Helpers
+{
+    public class MathChallengeGenerator
+    {
+        public static MathChallenge GenerateMathChallenge()
+        {
+            Random random = new Random();
+            var o1 = random.Next(1, 11);
+            var o2 = random.Next(1, 11);
+            var operation = random.Next(1, 5);
+            string operatorString = "+";
+            int answer;
+
+            switch (operation)
+            {
+                case 1:
+                    answer = o1 + o2;
+                    operatorString = "+";
+                    break;
+                case 2:
+                    answer = o1 - o2;
+                    operatorString = "-";
+                    break;
+                case 3:
+                    answer = o1 * o2;
+                    operatorString = "*";
+                    break;
+                case 4:
+                    answer = o1 / o2;
+                    operatorString = "/";
+                    break;
+                default:
+                    answer = 0;
+                    operatorString = "?";
+                    break;
+            }
+
+            string mathChallegeString = $"{o1} {operatorString} {o2}";
+            return new MathChallenge(){ Challenge = mathChallegeString, Answer = answer};
+        }
+    }
+}
---
+++ GrabCAD.API/Models/MathChallenge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrabCAD.API.Models
+{
+    public class MathChallenge
+    {
+        public string Challenge { get; set; }
+
+        public int Answer { get; set; }
+    }
+}
