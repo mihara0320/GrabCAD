9f2b3c96500e56521f4eba166a0b8eac0d077486
Masaki <masaki0320@icloud.com>
2019-02-26 01:37:09 +0200

    mathChallenge model

--- GrabCAD.API/Models/MathChallenge.cs
+++ GrabCAD.API/Models/MathChallenge.cs
@@ -9,6 +9,12 @@ namespace GrabCAD.API.Models
     {
         public string Challenge { get; set; }
 
-        public int Answer { get; set; }
+        public int PotentialAnswer { get; set; }
+
+        public int CorrectAnswer { get; set; }
+
+        public bool Answer { get; set; } // Answer is YES or NO
+
+        public bool AnswerFound { get; set; } = false;
     }
 }
