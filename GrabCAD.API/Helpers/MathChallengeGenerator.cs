4ad00fc33023d04a2a4ad8358e06bb2631a70754
Masaki <masaki0320@icloud.com>
2019-02-26 01:36:23 +0200

    Math Challenge helper added

--- GrabCAD.API/Helpers/MathChallengeGenerator.cs
+++ GrabCAD.API/Helpers/MathChallengeGenerator.cs
@@ -10,39 +10,51 @@ namespace GrabCAD.API.Helpers
     {
         public static MathChallenge GenerateMathChallenge()
         {
-            Random random = new Random();
+            var random = new Random();
             var o1 = random.Next(1, 11);
             var o2 = random.Next(1, 11);
             var operation = random.Next(1, 5);
-            string operatorString = "+";
-            int answer;
+            var deviant = random.Next(-1, 2);
+            string operatorString;
+            int correctAnswer;
 
             switch (operation)
             {
                 case 1:
-                    answer = o1 + o2;
+                    correctAnswer = o1 + o2;
                     operatorString = "+";
                     break;
                 case 2:
-                    answer = o1 - o2;
+                    correctAnswer = o1 - o2;
                     operatorString = "-";
                     break;
                 case 3:
-                    answer = o1 * o2;
+                    correctAnswer = o1 * o2;
                     operatorString = "*";
                     break;
                 case 4:
-                    answer = o1 / o2;
+                    correctAnswer = o1 / o2;
                     operatorString = "/";
                     break;
                 default:
-                    answer = 0;
+                    correctAnswer = 0;
                     operatorString = "?";
                     break;
             }
 
-            string mathChallegeString = $"{o1} {operatorString} {o2}";
-            return new MathChallenge(){ Challenge = mathChallegeString, Answer = answer};
+            var potentialAnswer = correctAnswer + deviant;
+            var mathChallegeString = $"{o1} {operatorString} {o2} = {potentialAnswer}";
+
+
+            return new MathChallenge()
+            {
+                Challenge = mathChallegeString,
+                PotentialAnswer = potentialAnswer,
+                CorrectAnswer = correctAnswer,
+                Answer = correctAnswer == potentialAnswer
+            };
+
+
         }
     }
 }
