36f58455ea66d26af040f84d9b333148d41c4da5
Masaki <masaki0320@icloud.com>
2019-02-26 18:03:30 +0200

    runs unit test on docker image build

---
+++ GrabCAD.API/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrabCAD.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json("default");
+        }
+    }
+}
