9371f5158099c7b99718e05288c0e3485e49e65c
Masaki <masaki0320@icloud.com>
2019-02-26 18:30:54 +0200

    init script added

---
+++ init.cmd
@@ -0,0 +1,2 @@
+cd %~dp0
+git submodule init && git submodule update
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# pwsh is assumed to be in the path
+# https://github.com/PowerShell/PowerShell
+
+git submodule init && git submodule update
